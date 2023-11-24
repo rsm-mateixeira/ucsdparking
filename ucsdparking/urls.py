86c1be9e560aa06cc65af71cba97af85b9e28c9c
mateixeira <mateixeira@ucsd.edu>
2023-11-24 15:16:03 -0800

    About changes

--- ucsdparking/urls.py
+++ ucsdparking/urls.py
@@ -5,7 +5,7 @@ app_name = "ucsdparking"
 
 urlpatterns = [
     path('', views.home_page_view, name='home'),
-    path('info', views.info_page_view, name='info'),
+    path('about', views.info_page_view, name='about'),
     path('map', views.map_page_view, name='map'),
     path('dashboards', views.dashboard_page_view, name='dashboards'),
     path('dashboards2', views.dashboard2_page_view, name='dashboards2'),
